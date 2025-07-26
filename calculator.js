19b89e4be99c0f2333343e94c461fcd1aaf96c19
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-07-26 23:32:49 +0600

    commit-4

--- calculator.js
+++ calculator.js
@@ -38,3 +38,18 @@ function calculator(a, b, operation) {
 
 const result = calculator(5, 7, 'add');
 console.log(result);
+
+const heights = [65, 66, 67, 89, 23, 44];
+
+function getMax(numbers) {
+    let max = Number.NEGATIVE_INFINITY; // Initialize with the smallest possible number
+    for (const num of numbers) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    return max;
+}
+
+const max = getMax(heights);
+console.log('Max value is:', max);
