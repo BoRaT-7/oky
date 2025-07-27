1338a09aab46a30b358214c7ebec37a7bf027664
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-07-27 22:57:32 +0600

    commit-5

--- variable/types.js
+++ variable/types.js
@@ -12,3 +12,26 @@ function getMax(numbers) {
 
 const max = getMax(heights);
 console.log('Max value is:', max);
+
+function sumofnumber (numbers){
+    let sum = 0;
+    for(const number of numbers){
+        console.log(number);
+        sum = sum + number;
+    }
+   return sum;
+
+}
+
+const numbs = [2,4,6,8]
+const sum = sumofnumber(numbs);
+console.log('Sum of number is', sum);
+
+
+const heights = [ 55, 78,12,40,99,100]
+
+function getMax(fullnumbers){
+    console.log(fullnumbers);
+}
+  const max = getMax (fullnumbers);
+console.log('max value is ', max);
