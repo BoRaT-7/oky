6ff95aeefd2c5afb7dd4c24758bb1d0811866036
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-07-28 12:56:00 +0600

    commit-6

---
+++ variable/argument.js
@@ -0,0 +1,11 @@
+let student1 = {name: 'nila', partner:'borsha'};
+let student2 = {name:'raj',partner:'anika'};
+
+function makemovie(couple1,couple2){
+    couple1.name ='onnoto';
+    couple2.partner = 'mim';
+}
+
+console.log(student1,student2);
+makemovie(student1,student2);
+console.log(student1,student2);
