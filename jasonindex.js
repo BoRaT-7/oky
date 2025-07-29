69745c018c4c6d6a606ec210e286aa1115239071
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-07-29 21:38:48 +0600

    commit-5

--- jasonindex.js
+++ jasonindex.js
@@ -9,3 +9,16 @@ function  loaduser() {
     .then(res => res.json())
     .then(data => console.log(data))
 }
+function loaduser2(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then (res => res.json())
+    .then (data => displayuser(data))
+}
+
+function displayuser(data){
+    for(const user of data){
+        console.log(user.name);
+       console.log(user.email);
+    }
+    
+}
